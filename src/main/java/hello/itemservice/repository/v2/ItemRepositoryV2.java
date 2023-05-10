46eb4cfb98f3ab90f863c0e9089d27e5b8d53bed
Dev-Suho <devsuho99@gmail.com>
2023-05-10 22:38:13 +0900

    Refactoring_0510

--- src/main/java/hello/itemservice/repository/v2/ItemRepositoryV2.java
+++ src/main/java/hello/itemservice/repository/v2/ItemRepositoryV2.java
@@ -1,5 +1,6 @@
 package hello.itemservice.repository.v2;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.itemservice.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
